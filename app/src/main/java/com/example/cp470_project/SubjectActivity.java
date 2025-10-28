5522dfaa1479555f651d0f0576ea3b5e2d7c4d43
land6430 <land6430@mylaurier.ca>
2025-10-28 18:33:35 -0400

    connected math and subjects, general trying to make look better too

--- app/src/main/java/com/example/cp470_project/SubjectActivity.java
+++ app/src/main/java/com/example/cp470_project/SubjectActivity.java
@@ -1,5 +1,6 @@
 package com.example.cp470_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,7 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class SubjectActivity extends AppCompatActivity {
+    private MaterialButton mathButton;
+    private MaterialButton englishButton;
+    private MaterialButton scienceButton;
+    private MaterialButton geographyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,16 @@ public class SubjectActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        mathButton = findViewById(R.id.math_button);
+        englishButton = findViewById(R.id.english_button);
+        scienceButton = findViewById(R.id.science_button);
+        geographyButton = findViewById(R.id.geography_button);
+
+        //math button clicked
+        mathButton.setOnClickListener(v-> {
+            Intent intent = new Intent(SubjectActivity.this, MathActivity.class);
+            startActivity(intent);
         });
     }
 }
