c17910695e3cb1371b5ea0b54e050d92e70cc74b
eldym <eldymoddys@gmail.com>
2025-10-29 17:09:39 -0400

    Toolbar

--- app/src/main/java/com/example/cp470_project/MathActivity.java
+++ app/src/main/java/com/example/cp470_project/MathActivity.java
@@ -1,16 +1,23 @@
 package com.example.cp470_project;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MathActivity extends AppCompatActivity {
     public String ACTIVITY_NAME = "MathActivity";
@@ -19,6 +26,16 @@ public class MathActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_math);
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        Drawable overflowIcon = toolbar.getOverflowIcon();
+        if (overflowIcon != null) {
+            overflowIcon.setTint(ContextCompat.getColor(this, android.R.color.black));
+        }
+
         Button mL1 = findViewById(R.id.mathL1);
         Button mL2 = findViewById(R.id.mathL2);
         Button mL3 = findViewById(R.id.mathL3);
@@ -96,5 +113,24 @@ public class MathActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public boolean onCreateOptionsMenu (Menu m) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, m );
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem mi) {
+        if (mi.getItemId() == R.id.action_home) {
+            Log.d("Toolbar", "Home selected");
+            Intent intent = new Intent(this, SubjectActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP); // just clears all previous activity stuff
+            startActivity(intent);
+        } else if (mi.getItemId() == R.id.action_about) {
+            Log.d("Toolbar", "About selected");
+            Toast toast = Toast.makeText(this , "Version 1.0, by xlandr, LiamSoup123, eldym, DiwanB, Marushen1366", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/cp470_project/MathL4Activity.java
+++ app/src/main/java/com/example/cp470_project/MathL4Activity.java
@@ -1,12 +1,21 @@
 package com.example.cp470_project;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MathL4Activity extends AppCompatActivity {
 
@@ -15,10 +24,39 @@ public class MathL4Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_math_l4);
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        Drawable overflowIcon = toolbar.getOverflowIcon();
+        if (overflowIcon != null) {
+            overflowIcon.setTint(ContextCompat.getColor(this, android.R.color.black));
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public boolean onCreateOptionsMenu (Menu m) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, m );
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem mi) {
+        if (mi.getItemId() == R.id.action_home) {
+            Log.d("Toolbar", "Home selected");
+            Intent intent = new Intent(this, SubjectActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP); // just clears all previous activity stuff
+            startActivity(intent);
+        } else if (mi.getItemId() == R.id.action_about) {
+            Log.d("Toolbar", "About selected");
+            Toast toast = Toast.makeText(this , "Version 1.0, by xlandr, LiamSoup123, eldym, DiwanB, Marushen1366", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        return true;
     }
 }
